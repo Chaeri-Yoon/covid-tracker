f4b7c8d42d55a06e67f7fb4d2c74ca165f8500a3
Chaeri <cryoon0103@gmail.com>
2022-01-06 02:33:18 +0000

    Create Loading Component using react-bootstrap

--- src/Components/Loading.tsx
+++ src/Components/Loading.tsx
@@ -1,6 +1,27 @@
+import styled from "styled-components";
+import Spinner from 'react-bootstrap/Spinner'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const SpinnerContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    width: 100%;
+    height: 100vh;
+
+    animation: spinner-opacity 1s linear infinite;
+    @keyframes spinner-opacity {
+        0% { opacity: 1; }
+        50% { opacity: 0; }
+        100% { opacity: 1; }
+    }
+`;
 function Loading() {
     return (
-        <>Loading</>
+        <SpinnerContainer>
+            <Spinner animation="border" variant="danger" />
+        </SpinnerContainer>
     );
 }
 export default Loading;
